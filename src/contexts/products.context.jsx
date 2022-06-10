cd8e966b1981832d8ee7903f16440aacd85a0ea8
Stefan Radovic <radovicstef@gmail.com>
2022-06-11 00:24:12 +0200

    Removed use effect adding shop data

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -12,10 +12,6 @@ export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
   const value = { products };
 
-  useEffect(() => {
-    addCollectionAndDocuments("categories", SHOP_DATA);
-  }, []);
-
   return (
     <ProductsContext.Provider value={value}>
       {children}
