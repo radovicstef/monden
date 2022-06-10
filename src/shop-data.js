05528961e2ad668b0c08c6586b38f87975328e5b
Stefan Radovic <radovicstef@gmail.com>
2022-06-11 00:23:30 +0200

    Added categories to fiestore database

---
+++ src/shop-data.js
@@ -0,0 +1,240 @@
+const SHOP_DATA = [
+  {
+    title: "hats",
+    women: {
+      items: [
+        {
+          id: 1,
+          name: "Dahila Boater",
+          imageURL:
+            "https://cdn.shopify.com/s/files/1/0431/5161/products/dahlia-boater-brown-felt-hats-3_800x.jpg?v=1623280329",
+          price: 95,
+        },
+        {
+          id: 2,
+          name: "Wes Fedora",
+          imageURL:
+            "https://cdn.shopify.com/s/files/1/0431/5161/products/wes-fedora-light-grey-felt-hats-4_800x.jpg?v=1622743628",
+          price: 89,
+        },
+        {
+          id: 3,
+          name: "Dahila Triangle",
+          imageURL:
+            "https://cdn.shopify.com/s/files/1/0431/5161/products/IMG_3207_800x.jpg?v=1631892816",
+          price: 110,
+        },
+        {
+          id: 4,
+          name: "Fiona Straw Fedora",
+          imageURL:
+            "https://cdn.shopify.com/s/files/1/0431/5161/products/fiona-straw-fedora-cream-straw-hats_800x.jpg?v=1623276006",
+          price: 95,
+        },
+        {
+          id: 5,
+          name: "Isla Straw Fedora",
+          imageURL:
+            "https://cdn.shopify.com/s/files/1/0431/5161/products/isla-straw-fedora-brown-straw-hats_800x.jpg?v=1623286616",
+          price: 95,
+        },
+        {
+          id: 6,
+          name: "River Guatemalan Palm",
+          imageURL:
+            "https://cdn.shopify.com/s/files/1/0431/5161/products/river-guatemalan-palm-oak-straw-hats_2000x.jpg?v=1622760540",
+          price: 196,
+        },
+      ],
+    },
+    men: {
+      items: [
+        {
+          id: 1,
+          name: "Florida Natural",
+          imageURL:
+            "https://www.raceuhats.com/7058-thickbox_default/straw-hat-mens-natural-fedora-style.jpg",
+          price: 65,
+        },
+        {
+          id: 2,
+          name: "Paros Panama",
+          imageURL:
+            "https://www.raceuhats.com/6766-thickbox_default/paros-panama-hat-man-brown-color.jpg",
+          price: 95,
+        },
+        {
+          id: 3,
+          name: "Nevada Wool",
+          imageURL:
+            "https://www.raceuhats.com/6318-thickbox_default/nevada-wool-felt-hat-fedora-style-for-men.jpg",
+          price: 95,
+        },
+        {
+          id: 4,
+          name: "Cuenca Panama",
+          imageURL:
+            "https://www.raceuhats.com/7050-thickbox_default/original-panama-hat-natural-with-brown-band-for-men.jpg",
+          price: 85,
+        },
+        {
+          id: 5,
+          name: "Arizona Wool",
+          imageURL:
+            "https://www.raceuhats.com/6303-thickbox_default/arizona-wool-felt-hat-for-men.jpg",
+          price: 105,
+        },
+        {
+          id: 6,
+          name: "Petra Wool",
+          imageURL:
+            "https://www.raceuhats.com/7080-thickbox_default/petra-brown-wool-felt-fedora-hat-for-men.jpg",
+          price: 100,
+        },
+      ],
+    },
+  },
+  {
+    title: "suits",
+    women: {
+      items: [
+        {
+          id: 1,
+          name: "High Rise Pant",
+          imageURL:
+            "https://anninc.scene7.com/is/image/AN/581204_3546_ALT2?$fullBpdp$",
+          price: 150,
+        },
+        {
+          id: 2,
+          name: "The One Button Blazer",
+          imageURL:
+            "https://anninc.scene7.com/is/image/AN/586221_8187_ALT2?$fullBpdp$",
+          price: 215,
+        },
+        {
+          id: 3,
+          name: "High Rise Ankle Pant",
+          imageURL:
+            "https://anninc.scene7.com/is/image/AN/590354_8818?$fullBpdp$",
+          price: 135,
+        },
+        {
+          id: 4,
+          name: "One Button Brazer",
+          imageURL:
+            "https://anninc.scene7.com/is/image/AN/581184_9263?$fullBpdp$",
+          price: 200,
+        },
+      ],
+    },
+    men: {
+      items: [
+        {
+          id: 1,
+          name: "Houndstoon Suit Pants",
+          imageURL:
+            "https://static.zara.net/photos///2022/V/0/2/p/4534/890/400/2/w/750/4534890400_2_4_1.jpg?ts=1651487658127",
+          price: 69,
+        },
+        {
+          id: 2,
+          name: "Birdseye Suit",
+          imageURL:
+            "https://static.zara.net/photos///2022/V/0/2/p/1564/420/401/102/w/750/1564420401_2_3_1.jpg?ts=1649757468453",
+          price: 155,
+        },
+        {
+          id: 3,
+          name: "Plaid Suit Jacket",
+          imageURL:
+            "https://static.zara.net/photos///2022/V/0/2/p/4259/744/986/2/w/750/4259744986_6_3_1.jpg?ts=1647256538943",
+          price: 149,
+        },
+        {
+          id: 4,
+          name: "Birdseye Suit Jacket",
+          imageURL:
+            "https://static.zara.net/photos///2022/V/0/2/p/1564/420/707/2/w/750/1564420707_6_3_1.jpg?ts=1650357876451",
+          price: 140,
+        },
+      ],
+    },
+  },
+  {
+    title: "shoes",
+    women: {
+      items: [
+        {
+          id: 1,
+          name: "Signature Leather Trainer",
+          imageURL:
+            "https://xcdn.next.co.uk/Common/Items/Default/Default/ItemImages/AltItemZoom/T10382s.jpg",
+          price: 90,
+        },
+        {
+          id: 2,
+          name: "Bronze Sandal",
+          imageURL:
+            "https://xcdn.next.co.uk/common/Items/Default/Default/Publications/G97/shotzoom/539/T40-452s.jpg",
+          price: 65,
+        },
+        {
+          id: 3,
+          name: "Comfort Wedges",
+          imageURL:
+            "https://xcdn.next.co.uk/Common/Items/Default/Default/ItemImages/AltItemZoom/M64663s.jpg",
+          price: 45,
+        },
+        {
+          id: 4,
+          name: "Lipsy Trainer",
+          imageURL:
+            "https://xcdn.next.co.uk/Common/Items/Default/Default/ItemImages/AltItemZoom/R62120s.jpg",
+          price: 60,
+        },
+      ],
+    },
+    men: {
+      items: [
+        {
+          id: 1,
+          name: "Brogue Shoes",
+          imageURL:
+            "https://images.asos-media.com/products/asos-design-brogue-shoes-in-brown-leather-with-natural-sole-and-colour-details/13929753-1-brown?$n_640w$&wid=513&fit=constrain",
+          price: 90,
+        },
+        {
+          id: 2,
+          name: "Walk London",
+          imageURL:
+            "https://images.asos-media.com/products/walk-london-capri-snaffle-loafers-in-brown-leather/201505008-1-brown?$n_640w$&wid=513&fit=constrain",
+          price: 105,
+        },
+        {
+          id: 3,
+          name: "Boat Shoes",
+          imageURL:
+            "https://images.asos-media.com/products/asos-design-boat-shoe-in-brown-suede-with-contrast-sole/201597809-1-brown?$n_640w$&wid=513&fit=constrain",
+          price: 65,
+        },
+        {
+          id: 4,
+          name: "Derby Shoes",
+          imageURL:
+            "https://images.asos-media.com/products/schuh-remi-lace-up-derby-shoes-in-brown-leather/201639493-1-brown?$n_640w$&wid=513&fit=constrain",
+          price: 99,
+        },
+        {
+          id: 5,
+          name: "Black Shoes",
+          imageURL:
+            "https://images.asos-media.com/products/asos-design-shoes-in-black-suede-with-apron-seam-detail/201783077-1-black?$n_960w$&wid=952&fit=constrain",
+          price: 60,
+        },
+      ],
+    },
+  },
+];
+
+export default SHOP_DATA;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -11,7 +11,14 @@ import {
   onAuthStateChanged,
 } from "firebase/auth";
 
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAqI-PVCPcme4Frn8RGiwCCw7J-uhtSJKQ",
@@ -42,6 +49,22 @@ export const signInWithGoogleRedirect = () => {
 
 //singleton instance of firestore database
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = collection(db, collectionKey);
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log("Done");
+};
 
 export const createUserDocumentFromAuth = async (userAuth) => {
   if (!userAuth) return;
